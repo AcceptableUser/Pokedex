30b0eeaeaa59448b7a32bf3de273c80528d16d14
Franco <Franco Gomez>
2020-11-20 11:44:39 -0300

    dark type pokemon added

--- scripts.js
+++ scripts.js
@@ -15,7 +15,8 @@ const colors = {
 	psychic: '#eaeda1',
 	flying: '#F5F5F5',
 	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+	normal: '#F5F5F5',
+	dark: '#919191'
 };
 const main_types = Object.keys(colors);
 
